344b3341f58d83263a8a678689040b1eea6e941c
Renan Silva <renaneduardo1@hotmail.com>
2025-03-27 23:19:35 -0300

    Revert "create title component"
    This reverts commit 23db584c8d32f4aceeb2227d448ce0fde6d03b3b.

--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -1,12 +1,11 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { Minimize2 } from 'lucide-react';
 import { Button } from './ui/button';
-import { UploadWidgetTitle } from './upload-widget-title';
 
 export function UploadWidgetHeader() {
 	return (
 		<div className='w-full p-4 py-2 bg-white/[0.02] border-zinc-800 border-b flex items-center justify-between'>
-			<UploadWidgetTitle />
+			<span className='text-sm font-medium'>Upload files</span>
 			<Collapsible.Trigger asChild>
 				<Button type='button' size='icon' className='-mr-2'>
 					<Minimize2 strokeWidth={1.5} className='size-4' />
